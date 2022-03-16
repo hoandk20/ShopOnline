4e44a81fa47c19803d42b9edb0e40a8200702424
HOANDK\Admin <hoandk20000@gmail.com>
2022-03-16 23:04:44 +0700

    add get list image

--- app/src/main/java/com/example/shoponline/Common/ImageSupport.java
+++ app/src/main/java/com/example/shoponline/Common/ImageSupport.java
@@ -3,6 +3,13 @@ package com.example.shoponline.Common;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import com.example.shoponline.Model.Product;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class ImageSupport {
@@ -14,6 +21,33 @@ public class ImageSupport {
         return content;
 
     }
+    public ArrayList<String> getListBase64ImageProduct(){
+        String Api = String.format(api.GetListBase64ImageProduct);
+        String content = api.GetStringFromApi(Api);
+        ArrayList<String> listBase64 = new ArrayList<>();
+        try {
+            JSONObject obj = new JSONObject(content);
+            JSONArray jsonArray = obj.getJSONArray("Image");
+            if (jsonArray != null) {
+
+                for (int i=0;i<jsonArray.length();i++){
+                    Product p = new Product();
+                    JSONObject o = new JSONObject();
+                    o = (JSONObject)jsonArray.getJSONObject(i);
+
+
+                    listBase64.add(o.getString("Data"));
+
+                }
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+      return listBase64;
+    }
 
     public Bitmap getBitMapImagebyId(long ImageId){
         String base64String = getLinkImageById(ImageId);
@@ -21,4 +55,18 @@ public class ImageSupport {
         Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         return decodedByte;
     }
+    public ArrayList<Bitmap> getListBitMapImageProduct(){
+        ArrayList<String> listS = getListBase64ImageProduct();
+        ArrayList<Bitmap> listBitmap =new ArrayList<>();
+        for (String i:listS
+             ) {
+            byte[] decodedString = Base64.getDecoder().decode(i);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            listBitmap.add(decodedByte);
+        }
+
+
+        return listBitmap;
+    }
+
 }
